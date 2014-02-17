6359226476d6724133863ec9a806d70c2ceaadb1
Esther Jun Kim <esther.jun@gmail.com>
2014-02-16 21:30:00 -0800

    Add comments to pairing section

--- routes/socket.js
+++ routes/socket.js
@@ -12,19 +12,20 @@ module.exports = function(socket) {
   // establish connection
   socket.emit('pair:init', {});
 
-  // receive device type
+  // pair mobile and PC
+  // Reference: http://blog.artlogic.com/2013/06/21/phone-to-browser-html5-gaming-using-node-js-and-socket-io/
   socket.on('pair:deviceType', function(data) {
     // if deviceType is 'pc', generate a unique code and send to PC
     if(data.deviceType == 'pc') {
       // generate a code
       var code = crypto.randomBytes(3).toString('hex');
 
-      // ensure uniqueness
+      // ensure code is unique
       while(code in socketCodes) {
         code = crypto.randomBytes(3).toString('hex');
       }
 
-      // store pairing code / socket assocation
+      // store code / socket assocation
       socketCodes[code] = this;
       socket.code = code;
 
@@ -35,13 +36,13 @@ module.exports = function(socket) {
     else if(data.deviceType == 'mobile') {
       socket.on('pair:getCode', function(data) {
         if(data.code in socketCodes) {
-          // save the code for controller commands
+          // save the code for mobile commands
           socket.code = data.code;
 
-          // initialize the controller
+          // start mobile connection
           socket.emit('pair:connected', {});
 
-          // start the PC
+          // start PC connection
           socketCodes[data.code].emit('pair:connected', {});
         }
         else {
